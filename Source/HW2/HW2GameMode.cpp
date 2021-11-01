2828f2a7b75797bef94dfd359d54ff724771c07b
den <den@DESKTOP-6SNMKIF>
2021-11-01 15:07:09 +0200

    Add char

--- Source/HW2/HW2GameMode.cpp
+++ Source/HW2/HW2GameMode.cpp
@@ -8,10 +8,5 @@
 AHW2GameMode::AHW2GameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-	// use our custom HUD class
-	HUDClass = AHW2HUD::StaticClass();
 }
